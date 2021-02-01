1d2c73a2365be0167fb92a2c96cf273d6b1ca738
Saad Rassam <smrassam@gmail.com>
2021-02-01 13:27:15 -0500

    loop through inventory and either add item if not found or update quantity of existing items

--- Main/solution.js
+++ Main/solution.js
@@ -16,8 +16,18 @@ var getProductIndex = function (name) {
     return undefined;
 };
 // 4)search the new inventory for that product name and return its index.
-//5)If the item is found, quantity of the product is added to the quantity of the same product in current inventory.
-//6)If the item is not found, the entire product (name and quantity) is added to the current inventory.
+for (var i = 0; i < arr2.length; i++) {
+    index = getProductIndex.call(arr1, arr2[i][1]);
+
+    //5)If the item is not found, the entire product (name and quantity) is added to the current inventory.
+    if (index === undefined) {
+        arr1.push(arr2[i]);
+    } else {
+        //6)If the item is found, quantity of the product is added to the quantity of the same product in current inventory.
+        arr1[index][0] += arr2[i][0]
+    }
+
+}
 //7)The updated inventory, arr1, is then sorted by product name 
 
 
